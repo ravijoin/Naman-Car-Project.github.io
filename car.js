8fe1c5ae76b57d05b8cc10a627f76e2f757c3ca3
ravijoin <namannj1456@gmail.com>
2022-06-03 21:32:25 +0530

    sound

--- car.js
+++ car.js
@@ -2,3 +2,4 @@ var audio = document.createElement('audio');
 audio.setAttribute('src', 'mycar.mp3'); 
 audio.loop=true;
 audio.play();    
+<iframe src="https://cross-origin.com/myvideo.html" allow="autoplay; fullscreen"></iframe>
